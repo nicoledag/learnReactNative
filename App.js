8c1a7bea6c164846b80e6084ce98ee7e387ee959
nicoledag <nnardella@gmail.com>
2020-09-29 15:23:12 -0400

    Add flatList

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
   const [people, setPeople] = useState([
@@ -16,14 +16,23 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <ScrollView>
+
+      <FlatList  
+        data={people}
+        renderItem={({ item }) => (
+          <Text style={styles.item}>{item.name}</Text>
+        )}
+      
+      />
+
+      {/* <ScrollView>
       { people.map(item => (
           <View key={item.key}>
             <Text style={styles.item}>{item.name}</Text>
           </View>
         )
       )}
-      </ScrollView>
+      </ScrollView> */}
 
     </View>
   );
