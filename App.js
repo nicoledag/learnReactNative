5a366cdb06b3dac3734bb55cbe952dfb4e917ba5
nicoledag <nnardella@gmail.com>
2020-09-29 14:16:27 -0400

    Add useState

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('nicole')
+  const [person, setPerson] = useState({ name: 'mario', age: 40 })
+
+  const clickHandler = () => {
+    setName('chun-li')
+    setPerson({ name: 'luigi', age: 45 })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name is {name}</Text>
+      <Text>His name is {person.name} and his age is {person.age}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title='change state' onPress={clickHandler}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +30,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    marginTop: 20
+  }
 });
+
