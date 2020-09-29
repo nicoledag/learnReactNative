45679beff9a5746fdf6dc138248c82ebd49d4c5e
nicoledag <nnardella@gmail.com>
2020-09-29 14:48:26 -0400

    Add list

--- App.js
+++ App.js
@@ -1,32 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('nicole')
-  const [age, setAge] = useState('30')
+  const [people, setPeople] = useState([
+    { name: 'nicole', key: '1' },
+    { name: 'yoshi', key: '2' },
+    { name: 'mario', key: '3' },
+    { name: 'luigi', key: '4' },
+    { name: 'peach', key: '5' },
+    { name: 'toad', key: '6' },
+    { name: 'bowser', key: '7' },
+  ])
 
-  const clickHandler = () => {
-    setName('')
-    setAge('')
-  }
 
   return (
     <View style={styles.container}>
-      <Text>Enter name:</Text>
-      <TextInput 
-        style={styles.input} 
-        placeholder='e.g. John Doe'
-        onChangeText={(val) => setName(val)}/>
+      <ScrollView>
+      { people.map(item => (
+          <View key={item.key}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        )
+      )}
+      </ScrollView>
 
-      <Text>Enter age:</Text>
-      <TextInput 
-        style={styles.input} 
-        placeholder='e.g. 99'
-        onChangeText={(val) => setAge(val)}/>
-
-      <Text>name: {name}, age: {age}</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -35,15 +33,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200,
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24,
   }
 });
 
